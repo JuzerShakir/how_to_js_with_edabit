addac9dcab359b39d990f84f5a8ab8b87d1dea17
Juzer Shakir <juzershakir.webdev@gmail.com>
2022-04-20 22:30:27 +0530

    Using bitwise operators

---
+++ easy/001.bitwise_operator/solution.js
@@ -0,0 +1,7 @@
+const bitwiseOR = (a, b) => a | b;
+const bitwiseXOR = (a, b) => a ^ b;
+const bitwiseAND = (a, b) => a & b;
+
+console.log(bitwiseAND(7, 12));
+console.log(bitwiseOR(7, 12));
+console.log(bitwiseXOR(7, 12));
